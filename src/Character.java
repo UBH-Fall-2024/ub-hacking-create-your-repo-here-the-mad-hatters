f14ddf271ec50567ea5a4a922d138f5ddf212c96
wthobika <wthobika@buffalo.edu>
2024-11-09 14:07:07 -0500

    Basic character class

---
+++ src/Character.java
@@ -0,0 +1,15 @@
+import java.awt.image.BufferedImage;
+
+public class Character{
+
+    public int x, y;
+    public int movement_speed;
+    public int health;
+    public int damage;
+    public String Name;
+
+    public String direction;
+    public BufferedImage left, right;
+    
+}
+
